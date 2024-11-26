0418b1d0d1a4ed741a0c001545c64b43137d14f8
NadaHamarsheh <114285391+NadaHamarsheh@users.noreply.github.com>
2024-11-26 12:41:57 +0200

    Update connectionString

--- CompanySystem/Infrastructure/DesignTimeCompanySystemContextFactory.cs
+++ CompanySystem/Infrastructure/DesignTimeCompanySystemContextFactory.cs
@@ -10,7 +10,7 @@ namespace Infrastructure
             var dbContextBuilder = new DbContextOptionsBuilder<CompanySystemContext>();
 
             var connectionString = "Server=localhost;Database=CompanySystemDB;" +
-                "User=SA;Password=NadaSQL100;TrustServerCertificate=True;MultipleActiveResultSets=True";
+                "User=;Password=;TrustServerCertificate=True;MultipleActiveResultSets=True";
 
             dbContextBuilder.UseSqlServer(connectionString);
             return new CompanySystemContext(dbContextBuilder.Options);
